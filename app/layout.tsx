6103ea2eec9077771683b3c641b074b28f0ff069
Billy Beck <billys.beck@gmail.com>
2025-05-17 20:19:40 -0400

    Add Raleway font
    added raleway font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
 //import React from 'react';
 import './global.css';
+import { Raleway } from 'next/font/google';
 
+const raleway = Raleway({
+  subsets: ['latin'],
+  variable: '--font-raleway',
+  display: 'swap',
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+})
 
 export default function RootLayout({
   children,
@@ -11,7 +19,7 @@ export default function RootLayout({
 {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={raleway.className}>{children}</body>
     </html>
   )
 }
